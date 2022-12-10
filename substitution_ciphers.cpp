1b01a3deb46aa01e7ffd86f1f4138fd77973eb61
mvmikhaylova <91223620+mvmikhaylova@users.noreply.github.com>
2022-12-10 14:28:17 +0300

    Update substitution_ciphers.cpp

--- substitution_ciphers.cpp
+++ substitution_ciphers.cpp
@@ -1,8 +1,15 @@
-//Шифрока, расшифрока и криптоанализ шифра простой замены, аффинного и аффинного рекурретного шифров. 
+//Шифровка, расшифровка и криптоанализ шифра простой замены, аффинного и аффинного рекуррентного 
+//Для выполнения криптоанализа в cmake-build-debug необходимо поместить english_quadgrams
 
 #include <iostream>
+#include <random>
 #include <vector>
 #include <string>
+#include <map>
+#include <cmath>
+#include <experimental/random>
+#include "cstdio"
+#include "fstream"
 
 int gcd (int a, int b, int & x, int & y)
 {
@@ -18,24 +25,27 @@ int gcd (int a, int b, int & x, int & y)
     return d;
 }
 
-std::string simple_substitute(bool to_encrypt, std::string alphabet, std::string message = "")
+std::string simple_substitute(bool to_encrypt, std::string alphabet, std::string message = "", std::vector<int> keyIn = {})
 {
-    std::cout << "Выполняю шифр простой замены" << std::endl;
+    //std::cout << "Выполняю шифр простой замены" << std::endl;
 
     int size_alphabet = alphabet.size();
-
-    std::cout << "Введите " << size_alphabet << " элементов перестановки" << std::endl;
     std::vector<int> keys(size_alphabet);
-    for(int i = 0; i<size_alphabet; i++)
-    {
-        std::cin >> keys[i];
-    }
+
+    if(keyIn.empty()) {
+
+        std::cout << "Введите " << size_alphabet << " элементов перестановки" << std::endl;
+        for (int i = 0; i < size_alphabet; i++) {
+            std::cin >> keys[i];
+        }
+
+    }else keys = keyIn;
 
     std::string to_return;
 
     if(to_encrypt)
     {
-        std::cout << "Выполняю зашифрование" << std::endl;
+        //std::cout << "Выполняю зашифрование" << std::endl;
         for(auto character: message)
         {
             int pos = std::find(alphabet.begin(), alphabet.end(), character) - alphabet.begin();
@@ -44,7 +54,7 @@ std::string simple_substitute(bool to_encrypt, std::string alphabet, std::string
         }
     }else
     {
-        std::cout << "Выполняю расшифрование" << std::endl;
+        //std::cout << "Выполняю расшифрование" << std::endl;
         for(auto character: message)
         {
             int pos = std::find(alphabet.begin(), alphabet.end(), character) - alphabet.begin();
@@ -58,17 +68,33 @@ std::string simple_substitute(bool to_encrypt, std::string alphabet, std::string
 
 }
 
-std::string affine(bool to_encrypt, std::string alphabet, std::string message = "")
+std::string affine(bool to_encrypt, std::string alphabet, std::string message = "", std::vector<int> keyIn = {})
 {
-    int size_alphabet = alphabet.size();
-    std::cout << "Выполняю Аффинный шифр" << std::endl;
-    std::cout << "Введите альфа и бета" << std::endl;
     int alpha, beta;
-    std::cin >> alpha >> beta;
+    int size_alphabet = alphabet.size();
+
+    if(keyIn.empty()) {
+
+        //std::cout << "Выполняю Аффинный шифр" << std::endl;
+        std::cout << "Введите альфа и бета" << std::endl;
+
+        std::cin >> alpha >> beta;
+        int tmp1, tmp2;
+
+        if(gcd(alpha, size_alphabet, tmp1, tmp2)!=1){
+            std::cout << "Ошибка: Значения альфа и размера алфавита не являются взаимно простыми" << std::endl;
+            return "";
+        }
+
+    }else {
+        alpha = keyIn[0];
+        beta = keyIn[1];
+    }
     std::string to_return;
+
     if(to_encrypt)
     {
-        std::cout << "Выполняю зашифрование" << std::endl;
+        //std::cout << "Выполняю зашифрование" << std::endl;
         for(auto elem: message)
         {
             int pos = std::find(alphabet.begin(), alphabet.end(), elem) - alphabet.begin();
@@ -78,7 +104,7 @@ std::string affine(bool to_encrypt, std::string alphabet, std::string message = 
         }
     }else
     {
-        std::cout << "Выполняю расшифрование" << std::endl;
+        //std::cout << "Выполняю расшифрование" << std::endl;
         int alphaRev;
         int szRev;
         gcd(alpha, size_alphabet, alphaRev, szRev);
@@ -95,18 +121,37 @@ std::string affine(bool to_encrypt, std::string alphabet, std::string message = 
     return to_return;
 }
 
-std::string affine_recursive(bool to_encrypt, std::string alphabet, std::string message = "")
+std::string affine_recursive(bool to_encrypt, std::string alphabet, std::string message = "", std::vector<int> keyIn = {})
 {
+
     int size_alphabet = alphabet.size();
-    std::cout << "Выполняю аффинный рекуррентный шифр" << std::endl;
-    std::cout << "Вставьте первую пару альфа и бета" << std::endl;
     int alphaFir, betaFir, alphaSec, betaSec;
-    std::cin >> alphaFir >> betaFir;
-    std::cout << "Вставьте вторую пару альфа и бета" << std::endl;
-    std::cin >> alphaSec >> betaSec;
+
+    if(keyIn.empty()) {
+
+        //std::cout << "Выполняю аффинный рекуррентный шифр" << std::endl;
+        std::cout << "Вставьте первую пару альфа и бета" << std::endl;
+
+        std::cin >> alphaFir >> betaFir;
+        std::cout << "Вставьте вторую пару альфа и бета" << std::endl;
+        std::cin >> alphaSec >> betaSec;
+    }else{
+        alphaFir = keyIn[0];
+        betaFir = keyIn[1];
+        alphaSec = keyIn[2];
+        betaSec = keyIn[3];
+    }
+
+    int tmp1, tmp2;
+
+    if(gcd(alphaFir, size_alphabet, tmp1, tmp2)!=1 || gcd(alphaSec, size_alphabet, tmp1, tmp2)!=1){
+        std::cout << "Ошибка: Значения альфа и размера алфавита не являются взаимно простыми" << std::endl;
+        return "";
+    }
+
     std::string to_return;
     if(to_encrypt){
-        std::cout << "Выполняю зашифрование" << std::endl;
+        //std::cout << "Выполняю зашифрование" << std::endl;
         for(int i = 0; i<message.size(); i++)
         {
             auto elem = message[i];
@@ -141,7 +186,7 @@ std::string affine_recursive(bool to_encrypt, std::string alphabet, std::string 
         }
 
     }else {
-        std::cout << "Выполняю расшифрование" << std::endl;
+        //std::cout << "Выполняю расшифрование" << std::endl;
         for(int i = 0; i<message.size(); i++)
         {
             auto elem = message[i];
@@ -180,9 +225,232 @@ std::string affine_recursive(bool to_encrypt, std::string alphabet, std::string 
     return to_return;
 }
 
+
+
+std::map<std::string, double> toCheckQuad;
+const double sumof = 4224127912;
+
+void map_init(){
+
+    auto f = freopen("english_quadgrams", "r", stdin);
+    for(int i = 0; i < 389373; i++) {
+
+        std::string inQuad;
+        int valueof;
+        std::cin >> inQuad >> valueof;
+        for (int k = 0; k<4; k++) {
+
+            char temp = tolower(inQuad[k]);
+            inQuad[k] = temp;
+
+        }
+
+        double toInsert = log10( ((float) valueof) / sumof);
+        toCheckQuad.insert({inQuad, toInsert});
+
+    }
+
+}
+
+double score(std::string text) {
+
+    double score = 0;
+    std::string ngrams = "english_quadgrams";
+
+    if(toCheckQuad.empty()) map_init();
+
+    double toround = log10(0.01 / sumof);
+
+    for(int i = 0; i < text.size(); i++) {
+        std::string nowAt;
+        nowAt.push_back(text[i]);
+        nowAt.push_back(text[i+1]);
+        nowAt.push_back(text[i+2]);
+        nowAt.push_back(text[i+3]);
+
+        if (toCheckQuad[nowAt] != 0){
+            score += toCheckQuad[nowAt];
+        }
+        else {
+            score += toround;
+        }
+    }
+    return score;
+}
+
+void cryptoanalysis_simple(std::string textIn, std::string alphabet) {
+
+    int i = 0;
+    std::vector<int> maxkey = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+    int maxscore = -100000000;
+    double parentscore = maxscore;
+    auto parentkey = maxkey;
+    for(int q = 0; q<200; q++)
+    {
+        i++;
+        std::shuffle(parentkey.begin(), parentkey.end(), std::mt19937(std::random_device()()));
+        auto deciphered = simple_substitute(0, alphabet, textIn, parentkey);
+        parentscore = score(deciphered);
+        int count = 0;
+        while (count < 1000)
+        {
+            int a = std::experimental::randint(0, 25);
+            int b = std::experimental::randint(0, 25);
+            auto child = parentkey;
+            std::swap(child[a], child[b]);
+            deciphered = simple_substitute(0, alphabet, textIn, child);
+            double scoreNow = score(deciphered);
+            if (scoreNow > parentscore) {
+                parentscore = scoreNow;
+                parentkey = child;
+                count = 0;
+            }
+            count = count + 1;
+        }
+        if (parentscore > maxscore) {
+            maxscore = parentscore;
+            maxkey = parentkey;
+            std::cout << "\nBest metrick: " << maxscore << "on iteration " << i << std::endl;
+            auto answer = simple_substitute(0, alphabet, textIn, maxkey);
+            std::cout << "Plain text: " << answer << std::endl;
+        }
+    }
+
+}
+
+void cryptoanalysis_affine(std::string textIn, std::string alphabet) {
+
+    int i = 0;
+    std::vector<int> maxkey = {1, 1};
+    int maxscore = -100000000;
+    double parentscore = maxscore;
+    auto parentkey = maxkey;
+    for (int k = 1; k<25; k++){
+        for (int j = 1; j<25; j++){
+
+
+            int tmp1 = 0;
+            int tmp2 = 0;
+
+            if (gcd(k, 26, tmp1, tmp2) != 1) {
+                continue;
+            }
+
+            i++;
+            auto deciphered = affine(0, alphabet, textIn, parentkey);
+            parentscore = score(deciphered);
+
+            std::vector<int> child = {k, j};
+            deciphered = affine(0, alphabet, textIn, child);
+            double scoreNow = score(deciphered);
+            if (scoreNow > parentscore) {
+                parentscore = scoreNow;
+                parentkey = child;
+            }
+
+            if (parentscore > maxscore) {
+                maxscore = parentscore;
+                maxkey = parentkey;
+                std::cout << "\nBest metrick: " << maxscore << "on iteration " << i << std::endl;
+                auto answer = affine(0, alphabet, textIn, maxkey);
+                std::cout << "Plain text: " << answer << std::endl;
+            }
+
+        }
+
+
+    }
+
+}
+
+
+void cryptoanalysis_affine_recurrent(std::string textIn, std::string alphabet) {
+
+    int i = 0;
+    std::vector<int> maxkey = {1, 1, 1, 1};
+    int maxscore = -100000000;
+    double parentscore = maxscore;
+    auto parentkey = maxkey;
+    for(int t = 1; t<25; t++) {
+        for (int q = 1; q < 25; q++) {
+            for (int k = 1; k < 25; k++) {
+                for (int j = 1; j < 25; j++) {
+
+
+                    int tmp1 = 0;
+                    int tmp2 = 0;
+
+                    if (gcd(k, 26, tmp1, tmp2) != 1 || gcd(t, 26, tmp1, tmp2) != 1) {
+                        continue;
+                    }
+
+                    i++;
+                    auto deciphered = affine_recursive(0, alphabet, textIn, parentkey);
+                    parentscore = score(deciphered);
+
+                    std::vector<int> child = {t, q, k, j};
+                    deciphered = affine_recursive(0, alphabet, textIn, child);
+                    double scoreNow = score(deciphered);
+                    if (scoreNow > parentscore) {
+                        parentscore = scoreNow;
+                        parentkey = child;
+                    }
+
+                    if (parentscore > maxscore) {
+                        maxscore = parentscore;
+                        maxkey = parentkey;
+                        std::cout << "\nBest metrick: " << maxscore << "on iteration " << i << std::endl;
+                        auto answer = affine_recursive(0, alphabet, textIn, maxkey);
+                        std::cout << "Plain text: " << answer << std::endl;
+                    }
+
+                }
+            }
+        }
+    }
+
+}
+
 int main() {
 
+    std::cout << "Выполняется: Лабораторная 1 по КМЗИ" << std::endl;
 
+    std::cout << "Хотите выполнить криптоанализ? (1)/(0)" << std::endl;
+
+    bool ans;
+    std::cin >> ans;
+    if(ans == 1) {
+        std::cout << "К какому алгоритму применяем криптоанализ?" << std::endl;
+        std::cout << "1 - простой замены, 2 - аффинный шифр, 3 - аффинный рекуррентный шифр" << std::endl;
+        int modeofAnalysys;
+        std::cin >> modeofAnalysys;
+        std::cout << "Введите сообщение для анализа:" << std::endl;
+        std::string msgToBreak;
+        std::cin >> msgToBreak;
+        auto english_alph = "abcdefghijklmnopqrstuvwxyz";
+
+        if (ans) {
+            switch (modeofAnalysys) {
+
+                case 1:
+                    cryptoanalysis_simple(msgToBreak, english_alph);
+                    break;
+                case 2:
+                    cryptoanalysis_affine(msgToBreak, english_alph);
+                    break;
+                case 3:
+                    cryptoanalysis_affine_recurrent(msgToBreak, english_alph);
+                default:
+                    std::cout << "Неверный ввод данных" << std::endl;
+
+                    //cryptoanalysis_simple("vpebqjsfovjovpvifipuwfsijdijibtfwzfoesipmfnqmjgfgxpnnqdijmeippeboejsjmmwvbqvifxfgpxbmpohvjnf", english_alph);
+                    //cryptoanalysis_affine("rtdhradqhirepraqhqkpippopjqhbpnaqknqkneppepcuppa", english_alph);
+                    //cryptoanalysis_affine_recurrent("lxbbiegbqgqpbuelsnjgkkbqbfkhjdrsehwpdtxliwvefpiqm", english_alph);
+
+            }
+            return 0;
+        }
+    }
     while(true)
     {
 
